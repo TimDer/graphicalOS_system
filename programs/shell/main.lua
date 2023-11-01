b79b1d6d655c4a044d05922fc3de9f9a5ba87ec5
TimDer <timderksen97@icloud.com>
2023-11-01 15:46:00 +0100

    Add the shell

--- programs/shell/main.lua
+++ programs/shell/main.lua
@@ -1,1 +1,21 @@
-shell.run("shell")
+shell.setDir("/")
+
+local whileShell = true
+
+shellCommandHistory = {}
+while whileShell do
+    term.setBackgroundColor( 32768 )
+    term.setTextColour( 16 )
+    write( shell.dir() .. "> " )
+    term.setTextColour( 1 )
+    
+    local command = read( nil, shellCommandHistory, shell.complete )
+    table.insert( shellCommandHistory, command )
+    if command == "exit" then
+        print("In order to exit the shell, close the window.")
+    elseif command == "clear" then
+        shell.run(command)
+    else
+        shell.run( command )
+    end
+end
