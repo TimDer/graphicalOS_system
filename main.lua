6fcdd16ce862504405055039fec326c7dc02deac
TimDer <timderksen97@icloud.com>
2023-11-02 20:47:58 +0100

    Remove Basic Functions

--- main.lua
+++ main.lua
@@ -5,7 +5,6 @@ local returnMethods = {}
 
 -- Basics
 returnMethods.graphicalOsRootSettings = require "/graphicalOS_system/apis/settings"
-returnMethods.basicFunctions = require "/graphicalOS_system/apis/basicFunctions"
 returnMethods.kernel = require "/graphicalOS_system/kernel"
 returnMethods.stopTheOs = require "/graphicalOS_system/apis/stop"
 
