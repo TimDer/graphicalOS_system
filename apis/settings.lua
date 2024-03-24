0fd91184ea5c4e9b959a79789ab465d6770cf669
TimDer <timderksen97@icloud.com>
2024-03-24 17:23:43 +0100

    transition to variable based functions

--- apis/settings.lua
+++ apis/settings.lua
@@ -5,7 +5,7 @@ graphicalOsRootSettings.programs_dir = "/graphicalOS_data/programs"
 graphicalOsRootSettings.user_profile_dir = "/graphicalOS_data/users"
 graphicalOsRootSettings.settings_start_dir = "/graphicalOS_data/system-programs/settings/start"
 
-function graphicalOsRootSettings.file_exists(name)
+graphicalOsRootSettings.file_exists = function(name)
     local f=io.open(name,"r")
     if f~=nil then
         io.close(f) 
@@ -15,13 +15,13 @@ function graphicalOsRootSettings.file_exists(name)
     end
 end
 
-function graphicalOsRootSettings.delete_file_exists(name)
+graphicalOsRootSettings.delete_file_exists = function(name)
     if graphicalOsRootSettings.file_exists(name) then
         fs.delete(name)
     end
 end
 
-function graphicalOsRootSettings.prepareGraphicalOsForBoot()
+graphicalOsRootSettings.prepareGraphicalOsForBoot = function()
     term.clear()
     term.setBackgroundColor(8)
     term.setTextColor(1)
