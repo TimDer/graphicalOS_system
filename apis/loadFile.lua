96cecbee85e543ac74d4fcf4f275cbfeaf0d38b3
TimDer <timderksen97@icloud.com>
2024-11-03 20:46:11 +0100

    Add program args functionality

--- apis/loadFile.lua
+++ apis/loadFile.lua
@@ -5,9 +5,17 @@ loadFilePrivate.settings = require "/graphicalOS_system/apis/settings"
 loadFilePrivate.requireModule = dofile("/rom/modules/main/cc/require.lua").make
 
 loadFilePrivate.getEnv = function (environment, pathToProgram)
-    environment.shell = shell
-    environment.multishell = multishell
-    environment.require, environment.package = loadFilePrivate.requireModule(environment, pathToProgram)
+    local newEnvironment = {}
+    newEnvironment.term = term
+    newEnvironment.shell = shell
+    newEnvironment.multishell = multishell
+    newEnvironment.require, newEnvironment.package = loadFilePrivate.requireModule(environment, pathToProgram)
+
+    for key, value in pairs(newEnvironment) do
+        if environment[key] == nil then
+            environment[key] = value
+        end
+    end
 
     return setmetatable(environment, { __index = _G })
 end
@@ -23,20 +31,57 @@ loadFilePrivate.readFile = function (pathToProgram)
     end
 end
 
-loadFile.startProgram = function (pathToProgram, env)
-    pathToProgram = "/" .. shell.resolveProgram(pathToProgram)
+loadFilePrivate.splitCommand = function (pathToProgram)
+    local results = {}
+    
+    local isBetweenQuotes = false
+    for commandMatch in string.gmatch(pathToProgram .. "\"", "(.-)\"") do
+        if isBetweenQuotes == true then
+            table.insert(results, commandMatch)
+        else
+            for quoteMatch in string.gmatch(commandMatch, "[^ \t]+") do
+                table.insert(results, quoteMatch)
+            end
+        end
 
-    local fileString = loadFilePrivate.readFile(pathToProgram)
-    local environment = loadFilePrivate.getEnv(env, pathToProgram)
+        isBetweenQuotes = not isBetweenQuotes
+    end
 
-    local func = load(
-        fileString,
-        "@" .. pathToProgram,
-        nil,
-        environment
-    )
+    return results
+end
 
-    func()
+loadFilePrivate.errorHandling = function (command, env)
+    if type(env) ~= "table" then
+        env = {}
+    end
+    if type(command) ~= "string" then
+        error("No such program: " .. type(command), 0)
+    end
+end
+
+loadFile.startProgram = function (command, env)
+    loadFilePrivate.errorHandling(command, env)
+
+    local commandList = loadFilePrivate.splitCommand(command)
+    local pathToProgram = commandList[1]
+
+    if shell.resolveProgram(pathToProgram) ~= nil then
+        pathToProgram = "/" .. shell.resolveProgram(pathToProgram)
+
+        local fileString = loadFilePrivate.readFile(pathToProgram)
+        local environment = loadFilePrivate.getEnv(env, pathToProgram)
+
+        local func = load(
+            fileString,
+            "@" .. pathToProgram,
+            nil,
+            environment
+        )
+
+        func(table.unpack(commandList, 2))
+    else
+        error("No such program: " .. command, 0)
+    end
 end
 
 return loadFile
